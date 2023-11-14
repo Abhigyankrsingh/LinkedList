f9f998bd968ff4bbc156e17c1b32a7a8b2e35fbe
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-11-14 22:22:22 +0530

    Recursive

--- search.cpp
+++ search.cpp
@@ -16,24 +16,43 @@ struct Node{
 };
 
 
-int searchin(Node* head, int x){
-     int pos = 1;
-    Node*curr = head;
-    while(curr != NULL){
-        if(curr->data == x){
-            return pos;
-        } else {
-            pos++;
+// int searchin(Node* head, int x){
+//      int pos = 1;
+//     Node*curr = head;
+//     while(curr != NULL){
+//         if(curr->data == x){
+//             return pos;
+//         } else {
+//             pos++;
 
-            curr = curr->next;
-        }
+//             curr = curr->next;
+//         }
 
       
-    }
-      return -1;
+//     }
+//       return -1;
     
-}
+// }
 
+
+
+int recsearch(Node* head, int x){
+
+    if(head == NULL) return -1;
+
+    if(head->data == x){
+        return 1;
+    } else {
+          int res = recsearch(head->next,  x);
+
+          if(res  == -1) return -1;
+
+          else {
+            return (res +1);
+          }
+    }
+
+}
 
 void printlist(Node* head){
     Node*curr = head;
@@ -54,7 +73,7 @@ head->next->next = new Node(4);
 
 // printlist(head);
 
-cout << searchin(head,2);
+cout << recsearch(head,2);
 return 0;
 
 }
